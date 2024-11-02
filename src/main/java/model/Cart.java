510f8e4a8b39e39a94381a1373ddc84fa9dfe9be
Sumit Sharma <s4063897@student.rmit.edu.au>
2024-11-02 22:52:23 +1100

    Modified cart class

--- src/main/java/model/Cart.java
+++ src/main/java/model/Cart.java
@@ -1,4 +1,42 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+    private static Cart instance;
+    private List<CartItem> cartItems;
+
+    private Cart() {
+        cartItems = new ArrayList<>();
+    }
+
+    public static synchronized Cart getInstance() {
+        if (instance == null) {
+            instance = new Cart();
+        }
+        return instance;
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void addOrUpdateItem(CartItem item) {
+        for (CartItem existingItem : cartItems) {
+            if (existingItem.getBookId() == item.getBookId()) {
+                existingItem.setQuantity(existingItem.getQuantity() + item.getQuantity());
+                return;
+            }
+        }
+        cartItems.add(item);
+    }
+
+    public void removeItem(int bookId) {
+        cartItems.removeIf(item -> item.getBookId() == bookId);
+    }
+
+    public void clearCart() {
+        cartItems.clear();
+    }
 }
